09634b8e125a499439abca0964b74aab488fe6f4
oshek <oshek14@freeuni.edu.ge>
2020-05-08 00:59:27 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EuElementsModule } from 'eu-elements';
+import { EuElementsModule } from '@oshek14/eu-elements';
 
 @NgModule({
   declarations: [
